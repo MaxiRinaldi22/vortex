a033d458539cbe624850f63fd2bf2b054f03fac6
Maxi <Mrinaldi2204@gmail.com>
2024-11-25 21:36:30 -0300

    Fix mobile design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,77 +89,79 @@ export default function Intro() {
     );
   }, []);
 
-  // Mover todo esto a un hero
+  // Mover todo esto a un hero (Acomodar todo para no usar el absolute y que pueda tener un h predefinido)
   return (
-    <section>
-      <div
-        className="absolute left-0 top-0 flex h-full w-full items-center justify-center"
-        ref={logoRef}
-        style={{
-          clipPath: "polygon(0% 50%, 0% 100%, 50% 100%, 0% 100%)",
-          overflowY: "hidden",
-        }}
-      >
-        <Image src="/MFM.png" alt="MFM" height={500} width={500} />
-      </div>
-      <div className="flex flex-col items-center justify-center gap-3">
-        <h1
-          ref={mainTextRef}
-          className="absolute left-1/2 top-[30%] flex w-full -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center text-center text-[40px] font-[200] text-white will-change-transform md:top-[36%] md:gap-4 md:p-0 md:text-7xl"
-        >
-          Nuestro objetivo es llevar tu negocio al
-          <br />
-          <span className="font-[600]"> siguente nivel</span>
-        </h1>
-
+  
+      <section >
         <div
-          ref={lineRef}
-          className="absolute left-1/2 top-[45%] -translate-x-1/2 -translate-y-1/2 transform md:top-[47%]"
+          className="absolute left-0 top-0 flex h-full w-full items-center justify-center"
+          ref={logoRef}
           style={{
-            clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+            clipPath: "polygon(0% 50%, 0% 100%, 50% 100%, 0% 100%)",
+            overflowY: "hidden",
           }}
         >
-          <div className="h-[2px] w-[300px] bg-main-default md:w-[520px]"></div>
+          <Image src="/MFM.png" alt="MFM" height={500} width={500} />
         </div>
-        <div
-          ref={blurRef}
-          className="absolute left-1/2 top-[45%] h-[4px] w-[300px] -translate-x-1/2 -translate-y-1/2 transform bg-main-default opacity-0 blur-[5px] filter md:top-[47%] md:w-[520px]"
-        ></div>
-      </div>
+        <div className="flex flex-col items-center justify-center gap-3">
+          <h1
+            ref={mainTextRef}
+            className="absolute left-1/2 top-[30%] flex w-full -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center text-center text-[40px] font-[200] text-white will-change-transform md:top-[36%] md:gap-4 md:p-0 md:text-7xl"
+          >
+            Nuestro objetivo es llevar tu negocio al
+            <br />
+            <span className="font-[600]"> siguente nivel</span>
+          </h1>
 
-      <div className="absolute left-1/2 top-[60%] flex w-full -translate-x-1/2 -translate-y-1/2 transform flex-col gap-4 px-5 md:top-[65%] md:gap-6">
-        <p
-          ref={workBtnRef}
-          className="w-full text-center text-lg font-[200] text-white will-change-transform md:text-2xl"
-        >
-          ¿Empezamos a trabajar juntos?
-        </p>
-        <ContactBtn />
-      </div>
-
-      <div className=" md:flex-row items-center justify-center md:pl-[328px] md:pr-[196px] md:justify-between absolute left-1/2 top-[85%] md:top-[92%] flex flex-col gap-5 w-full -translate-x-1/2 -translate-y-1/2 transform px-5">
-        <h2 className="md:items-start flex w-full md:w-80 justify-center md:justify-start md:text-2xl text-base font-[400] text-neutral-600">
-          Clientes que han confiado en nosotros
-        </h2>
-        <div className="flex md:justify-end justify-center items-center  md:pr-32 gap-5  w-full ">
-          {CLIENTS.map((elements) => {
-            return (
-              <div
-                key={elements.alt}
-                className="flex   items-center justify-center"
-              >
-                {/* PONER A TODOS LOS LOGOS CON EL MISMO TAMANO */}
-                <Image
-                  src={elements.img}
-                  alt={elements.alt}
-                  height={elements.scale}
-                  width={elements.scale}
-                />
-              </div>
-            );
-          })}
+          <div
+            ref={lineRef}
+            className="absolute left-1/2 top-[45%] -translate-x-1/2 -translate-y-1/2 transform md:top-[47%]"
+            style={{
+              clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+            }}
+          >
+            <div className="h-[2px] w-[300px] bg-main-default md:w-[520px]"></div>
+          </div>
+          <div
+            ref={blurRef}
+            className="absolute left-1/2 top-[45%] h-[4px] w-[300px] -translate-x-1/2 -translate-y-1/2 transform bg-main-default opacity-0 blur-[5px] filter md:top-[47%] md:w-[520px]"
+          ></div>
         </div>
-      </div>
-    </section>
+
+        <div className="absolute left-1/2 top-[68%] flex w-full -translate-x-1/2 -translate-y-1/2 transform flex-col gap-4 px-5 md:top-[65%] md:gap-6">
+          <p
+            ref={workBtnRef}
+            className="w-full text-center text-lg font-[200] text-white will-change-transform md:text-2xl"
+          >
+            ¿Empezamos a trabajar juntos?
+          </p>
+          <ContactBtn />
+        </div>
+
+        <div className="absolute left-1/2 top-[95%] flex w-full -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center gap-5 px-5 md:top-[92%] md:flex-row md:justify-between md:pl-[328px] md:pr-[196px]">
+          <h2 className="flex w-full justify-center text-base font-[400] text-neutral-600 md:w-80 md:items-start md:justify-start md:text-xl">
+            Clientes que han confiado en nosotros
+          </h2>
+          <div className="flex w-full items-center justify-center gap-5 md:justify-end md:pr-32">
+            {CLIENTS.map((elements) => {
+              return (
+                <div
+                  key={elements.alt}
+                  className="flex items-center justify-center"
+                >
+                  {/* PONER A TODOS LOS LOGOS CON EL MISMO TAMANO */}
+                  <Image
+                    src={elements.img}
+                    alt={elements.alt}
+                    height={elements.scale}
+                    width={elements.scale}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      <section className="h-screen mt-[90vh] w-full border"></section>
+      </section>
   );
 }
--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -4,7 +4,7 @@ import dr from "/public/clientes/dr.png";
 import LPD from "/public/clientes/LPD.png";
 
 export const CLIENTS: CLIENTES_TYPE[] = [
-  { img: gf, alt: "GF Barber Studio logo", scale: 90 },
-  { img: dr, alt: "DR Construcciones en steel logo", scale: 120 },
-  { img: LPD, alt: "Liceo Padre Domingo logo", scale: 60 },
+  { img: gf, alt: "GF Barber Studio logo", scale: 70 },
+  { img: dr, alt: "DR Construcciones en steel logo", scale: 100 },
+  { img: LPD, alt: "Liceo Padre Domingo logo", scale: 44 },
 ];
